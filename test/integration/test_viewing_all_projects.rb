c3d2a18e62e57eeeec0181d980fd2c702649a841
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-12 17:22:31 -0500

    Added test re: viewing all projects.

---
+++ test/integration/test_viewing_all_projects.rb
@@ -0,0 +1,53 @@
+require_relative '../test_helper'
+
+# In order to see a list of all the projects in my idea bank, the program should print a list of project names.
+
+# > ./idea_bank manage
+# 1. Add project
+# 2. Update project
+# 3. Delete project
+# 4. View all projects
+# > 4
+# My Projects
+# 1. [project name]
+# 2. [project name 2]
+# 3. [project name 3]
+# > 1
+# [project name]
+# Details: This is an example user input of project description.
+# Stack: Ruby
+# Back? (y/n)
+# > y
+# My Projects
+# 1. [project name]
+# 2. [project name 2]
+# 3. [project name 3]
+
+
+# Acceptance Criteria:
+# * User selects `4` from the main menu.
+# * A list of projects in the database is generated.
+# * User can select a project from the list to view it.
+# * User can return back to the main list of projects.
+
+class ViewingAllProjects < Minitest::Test
+
+  def test_viewing_no_projects
+    shell_output = ""
+    expected_output = ""
+    IO.popen("./idea_bank manage", "r+") do |pipe|
+      expected_output = <<EOS
+1. Add project
+2. Update project
+3. Delete project
+4. View all projects
+EOS
+      pipe.puts "4"
+      expected_output << "My Projects\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected_output, shell_output
+  end
+
+end
