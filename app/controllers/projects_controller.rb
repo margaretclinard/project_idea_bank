9a18b12a4bcff93f8aaf0705580b68933e8ba026
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-13 17:06:01 -0500

    Refactored ProjectsController to output a string.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,24 @@
+require 'highline/import'
+
 class ProjectsController
   def index
     if Project.count > 0
       projects = Project.all # All of the projects in an array
+      projects_string = ""
       projects.each_with_index do |project, index|
-        say("#{index + 1}. #{project.name}")
+        projects_string << "#{index + 1}. #{project.name}\n"
       end
+      projects_string
     else
-      say("No projects found. Add a project.\n")
+      "No projects found. Add a project.\n"
+    end
+  end
+
+  def add(name)
+    name_cleaned = name.strip
+    unless /^\d+$/.match(name_cleaned)
+      Project.create(name_cleaned)
+      name_cleaned
     end
   end
 end
