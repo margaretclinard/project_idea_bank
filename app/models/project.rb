ea2b053257cc50f1f8092a1874cd8632bc52d828
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-20 07:32:46 -0500

    Added find method and updated save method.

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,10 +18,24 @@ class Project
     Database.execute("select count(id) from projects")[0][0]
   end
 
+  def self.find(id)
+    row = Database.execute("select * from projects where id = ?", id).first
+    if row.nil?
+      nil
+    else
+      populate_from_database(row)
+    end
+  end
+
   def save
     return false unless valid?
-    Database.execute("INSERT INTO projects (name) VALUES (?)", name)
-    @id = Database.execute("SELECT last_insert_rowid()")[0]['last_insert_rowid()']
+    if @id.nil?
+      Database.execute("INSERT INTO projects (name) VALUES (?)", name)
+      @id = Database.execute("SELECT last_insert_rowid()")[0]['last_insert_rowid()']
+    else
+      Database.execute("UPDATE projects SET name=? WHERE id=?", name, id)
+      true
+    end
   end
 
   def valid?
@@ -32,5 +46,14 @@ class Project
       @errors = nil
       true
     end
+  end
+
+  private
+
+  def self.populate_from_database(row)
+    project = Project.new
+    project.name = row['name']
+    project.instance_variable_set(:@id, row['id'])
+    project
   end
 end
